145c5096ffddf067525f00a3736b8a0741f4ca2f
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-15 21:12:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,8 @@ class Shoe
 
 def initialize(brand)
     @brand = brand	   
-    if BRANDS << brand.include?(brand) == false 
+    if BRANDS.include?(brand) == false 
+      BRANDS << @brand 
       
 end 
   def cobble
